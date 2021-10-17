1834d54ac1fffca697eda51bd78164e0e23094c6
Madison Yu-Ying <89174083+madisonyuying@users.noreply.github.com>
2021-10-17 17:05:17 -0500

    finished mini project one

--- youtube-project-python/5-mini-projects/mini-one.py
+++ youtube-project-python/5-mini-projects/mini-one.py
@@ -1,1 +1,31 @@
+print("Welcome to my reptile quiz")
 
+playing = input("Do you want to play the quiz? ")
+
+if playing.lower() != "yes":
+    quit()
+print("Okay! Let's play!")
+score = 0 
+
+answerOne = input("How many types of scales do snakes have? ")
+if answerOne.lower() == "three" :
+    print("Correct!")
+    score+=1 
+else: 
+    print("incorrect!")
+
+answerTwo = input("Are there venomous of poisonous snakes? ")
+if answerTwo.lower() == "venomous":
+    print("Correct!")
+    score+=1
+else: 
+    print("incorrect!")
+answerThree = input("Are corn snakes rat snakes? ")
+if answerThree.lower() == "yes" :
+    print("Correct!")
+    score+=1 
+else: 
+    print("incorrect!")
+
+print("You got " +  str(score) + " questions correct!" )
+print("You got " +  str((score/3)*100) + " %!" )
