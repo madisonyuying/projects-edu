0a781f3ba735879e20b816af37586752cb0374a6
Madison Yu-Ying <89174083+madisonyuying@users.noreply.github.com>
2021-10-18 16:37:07 -0500

    completed mini project two

--- youtube-project-python/5-mini-projects/mini-two.py
+++ youtube-project-python/5-mini-projects/mini-two.py
@@ -1,1 +1,40 @@
+# number guesser projects
+import random 
+# importing random module 
 
+# rand_num = random.randrange(-7,8) # randrange does not include 8 but if i used randint it would include 8 
+guesses = 0
+
+top_of_range = input("Type a number: ")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than zero next time.")
+        quit()
+else: 
+    print("Please type a number next time.")
+    quit()
+    
+
+rand_num = random.randint(0,top_of_range)
+# print(rand_num) # printing a random number that was generated 
+
+
+while True:
+    guesses += 1 
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else: 
+        print("Please type a number next time.")
+        continue
+    if user_guess == rand_num:
+        print("You guessed correctly")
+        break 
+    elif user_guess > rand_num:
+        print("You guessed above the number")
+    elif user_guess < rand_num: 
+        print("You guessed below the number")
+
+print("You got it in", guesses, "guess(es)")
