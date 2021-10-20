39ababb720e90d5a7a77f849ac9cff733bc6a52c
Madison Yu-Ying <madisonyuying@gmail.com>
2021-10-20 06:49:13 -0500

    init commit for sorting hat

--- codecademy-cpp/harry-potter-sorting-quiz.cpp
+++ codecademy-cpp/harry-potter-sorting-quiz.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+int main() {
+    int gryffindor = 0, hufflepuff = 0, ravenclaw = 0, slytherin = 0;
+    int answer1, answer2, answer3, answer4;
+    std::cout << "=====================\n";
+std::cout << "The Sorting Hat Quiz!\n";
+std::cout << "=====================\n\n";
+}
