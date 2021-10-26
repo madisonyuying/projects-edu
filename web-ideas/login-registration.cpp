f8cfed79f0964efc14b88a4ffdee7b2185f644f9
Madison Yu-Ying <89174083+madisonyuying@users.noreply.github.com>
2021-10-25 19:31:00 -0500

    first version commit

--- web-ideas/login-registration.cpp
+++ web-ideas/login-registration.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(){
+    string username = "";
+    string password = "";
+    bool loginSuccess = false;
+
+    cout << "\tWelcome! Login below.\n\n";
+    do{
+        cout << "Username:  ";
+        cin >> username;
+        cout << "Password:  ";
+        cin >> password;
+
+        if (username == "username123" && password == "password123" ){
+            cout << "Successful login\n\n";
+            loginSuccess = true;
+        } else {
+            cout << "Incorrect username-password combination \n";
+            cout << "Please try login again.";
+        }
+    }while(!loginSuccess);
+
+
+
+
+
+    system("pause");
+    return 0 ;
+}
